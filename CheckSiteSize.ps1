1f97c7b4f44e5a8362309fd24b3fa59044aeefcf
ml394 <matt@webkolektiv.com>
2018-04-08 02:19:30 +0800

    check site size script

---
+++ CheckSiteSize.ps1
@@ -0,0 +1,27 @@
+﻿Param(
+    [Parameter(Mandatory=$true)]
+    [string]$rootURL,
+    [string]$greaterThan
+)
+
+Add-PSSnapin Microsoft.SharePoint.PowerShell
+
+$spAssignment = Start-SPAssignment
+
+$webApp = Get-SPWebApplication $rootURL
+
+if (! $greaterThan) {
+    $greaterThan = 30
+}
+
+foreach ($site in $webApp.Sites) {
+    $SizeInKB = $Site.Usage.Storage
+    $SizeInGB = [math]::Round($SizeInKB/1024/1024/1024,2)
+    if ($SizeInGB -gt $greaterThan) {
+        Write-Output $site.RootWeb.Title, $SizeInGB
+    }
+    $site.Dispose()
+}
+
+
+Stop-SPAssignment $spAssignment
